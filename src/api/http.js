0f85372d4da5be2e9e098c34a88d4b728180bb72
Ramon <rgah2107@gmail.com>
2026-04-20 20:02:31 +0300

    feat: add env conf

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'https://pruebareactjs.test-class.com/Api';
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'https://pruebareactjs.test-class.com/Api';
 
 export function createHttpClient({ getAuthToken, onUnauthorized } = {}) {
   const client = axios.create({
